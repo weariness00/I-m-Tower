47ff8d1e8bdc48f8cb45fde48a938096a08ce3b8
Seo Down Woo <112839880+weariness00@users.noreply.github.com>
2025-04-19 00:19:14 +0900

    Monster BT 구현 중

---
+++ Assets/Scripts/12.Unit/Monster/BT/MonsterStateType.cs
@@ -0,0 +1,12 @@
+using Unity.Behavior;
+
+namespace Unit.Monster.BT
+{
+	[BlackboardEnum]
+	public enum MonsterStateType
+	{
+		Idle,
+		Move,
+		Attack
+	}
+}
--- Assets/Scripts/12.Unit/Monster/MonsterControl.cs
+++ Assets/Scripts/12.Unit/Monster/MonsterControl.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Unity.Behavior;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,12 +7,14 @@ namespace Unit.Monster
 {
     public class MonsterControl : UnitBase
     {
-        [HideInInspector]public NavMeshAgent navMeshAgent;
+        [HideInInspector] public BehaviorGraphAgent behaviorGraphAgent;
+        [HideInInspector] public NavMeshAgent navMeshAgent;
         [NonSerialized] public new MonsterStatus status;
 
         public override void Awake()
         {
             base.Awake();
+            behaviorGraphAgent = GetComponent<BehaviorGraphAgent>();
             navMeshAgent = GetComponent<NavMeshAgent>();
             status = base.status as MonsterStatus;
         }
